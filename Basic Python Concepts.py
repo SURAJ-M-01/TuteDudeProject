869863a1f1992b41212130662a63aa9e66419f65
SURAJ-M-01 <surajmaske294Yahoo.com>
2025-09-04 19:53:44 +0530

    Initial commit

---
+++ Basic Python Concepts.py
@@ -0,0 +1,21 @@
+print("TASK 1")
+print("#Basic Mathematical Operations\n")
+
+
+c=input("Enter the first number: ")
+c=float(c)
+
+d=input("Enter the second number: ")
+d=float(d)
+
+print('\n')
+
+print("Addition =",c+d)
+
+print("Subtraction =",c-d)
+
+print("Multiplication =",c*d)
+
+print("Division =",c/d)
+
+
---
+++ numeric.py
@@ -0,0 +1,7 @@
+#print(max(1, 2, 3, 4, 5,15))
+
+#print(min(45,46,29,12,33))
+
+#print(abs(-3654))
+
+print(pow(4,4))
