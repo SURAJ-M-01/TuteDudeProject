ce698ba6c59ec940c5bbbd018a49eb56a26083f3
SURAJ-M-01 <surajmaske294Yahoo.com>
2025-09-10 16:45:42 +0530

    latest

---
+++ Personalised Greetings.py
@@ -0,0 +1,13 @@
+print("TASK 2")
+print("#Personalised Greetings\n")
+
+
+# Asking the user for their first name
+first_name = input("Enter your first name: ")
+
+# Asking the user for their last name
+last_name = input("Enter your last name: ")
+print("\n")
+
+# Printing a welcome message
+print("Hello, " + first_name + " " + last_name + "! Welcome to the Python program.")
